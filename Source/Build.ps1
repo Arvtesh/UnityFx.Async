9ca399a074b204999b99b1c98c3bb3864604f495
Alexander Bogarsukov <a.bogarsukov@gmail.com>
2017-08-06 10:56:33 +0300

    add AppVeyor support

--- Source/Build.ps1
+++ Source/Build.ps1
@@ -2,20 +2,25 @@ $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $solutionPath = Join-Path $scriptPath "UnityFx.sln"
 $configuration = $args[0]
 $outputPath = Join-Path $scriptPath "..\Build"
-$binPath = Join-Path $outputPath (Join-Path "Bin" $configuration)
+$binPath = Join-Path $outputPath "Bin"
 $binPath35 = Join-Path $binPath "net35"
 $binPath46 = Join-Path $binPath "net46"
 $assetsPath35 = Join-Path $scriptPath "..\AssetStore\UnityPackage35\Assets\UnityFx"
 $assetsPath46 = Join-Path $scriptPath "..\AssetStore\UnityPackage46\Assets\UnityFx"
-$assetsPathTests = Join-Path $scriptPath "..\AssetStore\UnityTests\Assets\UnityFx"
+$assetsPathTests = Join-Path $scriptPath "..\Tests\UnityTests\Assets\UnityFx"
 $msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MsBuild.exe"
 $nugetPath = Join-Path $outputPath "nuget.exe"
 $gitversionPath = Join-Path $outputPath "gitversion.commandline\tools\gitversion.exe"
 
 # create output folders if needed
-if (!(Test-Path $outputPath))
+if (!(Test-Path $binPath35))
 {
-	New-Item $outputPath -ItemType Directory
+	New-Item $binPath35 -ItemType Directory
+}
+
+if (!(Test-Path $binPath46))
+{
+	New-Item $binPath46 -ItemType Directory
 }
 
 # download nuget.exe if not present
@@ -53,5 +58,9 @@ $filesToPublish46 =
 	(Join-Path $scriptPath (Join-Path "UnityFx.Async\bin" (Join-Path $configuration "net46\UnityFx.Async.xml")))
 
 Copy-Item -Path $filesToPublish35 -Destination $assetsPath35 -Force
+Copy-Item -Path $filesToPublish35 -Destination $binPath35 -Force
 Copy-Item -Path $filesToPublish46 -Destination $assetsPath46 -Force
+Copy-Item -Path $filesToPublish46 -Destination $binPath46 -Force
 Copy-Item -Path $filesToPublish46 -Destination $assetsPathTests -Force
+Copy-Item -Path CHANGELOG.md -Destination $binPath -Force
+Copy-Item -Path README.md -Destination $binPath -Force
