b29032e9ade88ba20b5716cfa9f2e0b7a93edacf
Alexander Bogarsukov <a.bogarsukov@gmail.com>
2018-02-04 11:08:24 +0200

    Added xunit tests project

---
+++ src/UnityFx.Async.Tests/Tests/SampleTests.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+
+namespace UnityFx.Async.Tests
+{
+	public class SampleTests
+	{
+	}
+}
