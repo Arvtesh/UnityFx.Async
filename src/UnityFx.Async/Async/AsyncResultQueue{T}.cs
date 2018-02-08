2a8d581e6264b64d0aca0ab5e7b05b0c6015ee38
Alexander Bogarsukov <a.bogarsukov@gmail.com>
2018-02-08 21:23:19 +0200

    AsyncResultQueue refactoring

--- src/UnityFx.Async/Async/AsyncResultQueue{T}.cs
+++ src/UnityFx.Async/Async/AsyncResultQueue{T}.cs
@@ -13,9 +13,9 @@ namespace UnityFx.Async
 	/// <threadsafety static="true" instance="true"/>
 	/// <seealso cref="AsyncResult"/>
 #if NET35
-	public class AsyncResultQueue<T> : ICollection<T> where T : AsyncResult
+	public class AsyncResultQueue<T> where T : AsyncResult
 #else
-	public class AsyncResultQueue<T> : ICollection<T>, IReadOnlyCollection<T> where T : AsyncResult
+	public class AsyncResultQueue<T> where T : AsyncResult
 #endif
 	{
 		#region data
@@ -28,10 +28,17 @@ namespace UnityFx.Async
 		#region interface
 
 		/// <summary>
+		/// Gets the number of operations contained in the queue.
+		/// </summary>
+		/// <seealso cref="MaxCount"/>
+		public int Count => _ops.Count;
+
+		/// <summary>
 		/// Gets or sets maximum queue size. Default is 0 (no constraints).
 		/// </summary>
 		/// <value>Maximum queue size. Zero means no constraints.</value>
 		/// <exception cref="ArgumentOutOfRangeException">Thrown if the <see cref="MaxCount"/> value is less than 0.</exception>
+		/// <seealso cref="Count"/>
 		public int MaxCount
 		{
 			get
@@ -53,17 +60,16 @@ namespace UnityFx.Async
 		/// Get an operation that is running currently or <see langword="null"/> if the queue is empty.
 		/// </summary>
 		/// <value>An operation that is running currently.</value>
-		public T Current => _ops.Count > 0 ? _ops[0] : null;
-
-		#endregion
-
-		#region ICollection
-
-		/// <inheritdoc/>
-		public int Count => _ops.Count;
-
-		/// <inheritdoc/>
-		public bool IsReadOnly => false;
+		public T Current
+		{
+			get
+			{
+				lock (_ops)
+				{
+					return _ops.Count > 0 ? _ops[0] : null;
+				}
+			}
+		}
 
 		/// <summary>
 		/// Adds a new operation to the end of the queue. Operation should have its status set to <see cref="AsyncOperationStatus.Created"/>.
@@ -90,11 +96,7 @@ namespace UnityFx.Async
 			lock (_ops)
 			{
 				_ops.Add(op);
-
-				if (_ops.Count == 1)
-				{
-					op.SetRunning();
-				}
+				OnOperationAdded(op);
 			}
 		}
 
@@ -104,33 +106,25 @@ namespace UnityFx.Async
 		/// <param name="op">The operation to remove. Can be <see langword="null"/>.</param>
 		/// <returns>Returns <see langword="true"/> if the operation is removed; <see langword="false"/> otherwise.</returns>
 		/// <seealso cref="Add(T)"/>
+		/// <seealso cref="Clear"/>
 		public bool Remove(T op)
 		{
 			lock (_ops)
 			{
-				return _ops.Remove(op);
+				if (_ops.Remove(op))
+				{
+					OnOperationRemoved(op);
+					return true;
+				}
 			}
+
+			return false;
 		}
 
-		/// <inheritdoc/>
-		public bool Contains(T op)
-		{
-			lock (_ops)
-			{
-				return _ops.Contains(op);
-			}
-		}
-
-		/// <inheritdoc/>
-		public void CopyTo(T[] array, int arrayIndex)
-		{
-			lock (_ops)
-			{
-				_ops.CopyTo(array, arrayIndex);
-			}
-		}
-
-		/// <inheritdoc/>
+		/// <summary>
+		/// Removes all operations from the queue.
+		/// </summary>
+		/// <seealso cref="Remove(T)"/>
 		public void Clear()
 		{
 			lock (_ops)
@@ -139,20 +133,44 @@ namespace UnityFx.Async
 			}
 		}
 
-		#endregion
-
-		#region IEnumerable
-
-		/// <inheritdoc/>
-		public IEnumerator<T> GetEnumerator()
+		/// <summary>
+		/// Returns the queue snapshot as array.
+		/// </summary>
+		/// <returns>An array containing the queue snapshot.</returns>
+		public T[] ToArray()
 		{
-			return _ops.GetEnumerator();
+			lock (_ops)
+			{
+				return _ops.ToArray();
+			}
 		}
 
-		/// <inheritdoc/>
-		IEnumerator IEnumerable.GetEnumerator()
+		/// <summary>
+		/// Called when an operation has been added to the queue.
+		/// </summary>
+		/// <param name="op">The new operation.</param>
+		/// <seealso cref="OnOperationRemoved(T)"/>
+		/// <seealso cref="Add(T)"/>
+		protected virtual void OnOperationAdded(T op)
 		{
-			return _ops.GetEnumerator();
+			if (_ops.Count > 0)
+			{
+				_ops[0].TrySetRunning();
+			}
+		}
+
+		/// <summary>
+		/// Called when an operation has been removed from the queue.
+		/// </summary>
+		/// <param name="op">The operation removed.</param>
+		/// <seealso cref="OnOperationAdded(T)"/>
+		/// <seealso cref="Remove(T)"/>
+		protected virtual void OnOperationRemoved(T op)
+		{
+			if (_ops.Count > 0)
+			{
+				_ops[0].TrySetRunning();
+			}
 		}
 
 		#endregion
@@ -163,11 +181,11 @@ namespace UnityFx.Async
 		{
 			lock (_ops)
 			{
-				_ops.Remove(sender as T);
+				var op = sender as T;
 
-				if (_ops.Count > 0)
+				if (_ops.Remove(op))
 				{
-					_ops[0].TrySetRunning();
+					OnOperationRemoved(op);
 				}
 			}
 		}
