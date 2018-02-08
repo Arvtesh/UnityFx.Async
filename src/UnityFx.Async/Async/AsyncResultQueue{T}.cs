311f8681e6c52586bc44ef3f37c8839d0740ef4c
Alexander Bogarsukov <a.bogarsukov@gmail.com>
2018-02-08 19:49:47 +0200

    AsyncResultQueue is now generic

--- src/UnityFx.Async/Async/AsyncResultQueue{T}.cs
+++ src/UnityFx.Async/Async/AsyncResultQueue{T}.cs
@@ -13,26 +13,26 @@ namespace UnityFx.Async
 	/// <threadsafety static="true" instance="true"/>
 	/// <seealso cref="AsyncResult"/>
 #if NET35
-	public class AsyncResultQueue : ICollection<AsyncResult>
+	public class AsyncResultQueue<T> : ICollection<T> where T : AsyncResult
 #else
-	public class AsyncResultQueue : ICollection<AsyncResult>, IReadOnlyCollection<AsyncResult>
+	public class AsyncResultQueue<T> : ICollection<T>, IReadOnlyCollection<T> where T : AsyncResult
 #endif
 	{
 		#region data
 
-		private int _maxOpsSize = 256;
-		private List<AsyncResult> _ops = new List<AsyncResult>();
+		private int _maxOpsSize = 0;
+		private List<T> _ops = new List<T>();
 
 		#endregion
 
 		#region interface
 
 		/// <summary>
-		/// Gets or sets maximum queue size.
+		/// Gets or sets maximum queue size. Default is 0 (no constraints).
 		/// </summary>
-		/// <value>Maximum queue size.</value>
-		/// <exception cref="ArgumentOutOfRangeException">Thrown if the <see cref="MaxSize"/> value is 0 or less.</exception>
-		public int MaxSize
+		/// <value>Maximum queue size. Zero means no constraints.</value>
+		/// <exception cref="ArgumentOutOfRangeException">Thrown if the <see cref="MaxCount"/> value is less than 0.</exception>
+		public int MaxCount
 		{
 			get
 			{
@@ -40,9 +40,9 @@ namespace UnityFx.Async
 			}
 			set
 			{
-				if (value <= 0)
+				if (value < 0)
 				{
-					throw new ArgumentOutOfRangeException(nameof(MaxSize), value, "MaxSize value should be > 0");
+					throw new ArgumentOutOfRangeException(nameof(MaxCount), value, "MaxCount value should be >= 0");
 				}
 
 				_maxOpsSize = value;
@@ -53,7 +53,7 @@ namespace UnityFx.Async
 		/// Get an operation that is running currently or <see langword="null"/> if the queue is empty.
 		/// </summary>
 		/// <value>An operation that is running currently.</value>
-		public AsyncResult Current => _ops.Count > 0 ? _ops[0] : null;
+		public T Current => _ops.Count > 0 ? _ops[0] : null;
 
 		#endregion
 
@@ -70,16 +70,16 @@ namespace UnityFx.Async
 		/// </summary>
 		/// <param name="op">The operation to enqueue.</param>
 		/// <exception cref="ArgumentNullException">Thrown if <paramref name="op"/> is <see langword="null"/>.</exception>
-		/// <exception cref="InvalidOperationException">Thrown if either queue size exceeds <see cref="MaxSize"/> value or the operation status is not <see cref="AsyncOperationStatus.Created"/>.</exception>
-		/// <seealso cref="Remove(AsyncResult)"/>
-		public void Add(AsyncResult op)
+		/// <exception cref="InvalidOperationException">Thrown if either queue size exceeds <see cref="MaxCount"/> value or the operation status is not <see cref="AsyncOperationStatus.Created"/>.</exception>
+		/// <seealso cref="Remove(T)"/>
+		public void Add(T op)
 		{
 			if (op == null)
 			{
 				throw new ArgumentNullException(nameof(op));
 			}
 
-			if (_ops.Count >= _maxOpsSize)
+			if (_maxOpsSize > 0 && _ops.Count >= _maxOpsSize)
 			{
 				throw new InvalidOperationException();
 			}
@@ -103,8 +103,8 @@ namespace UnityFx.Async
 		/// </summary>
 		/// <param name="op">The operation to remove. Can be <see langword="null"/>.</param>
 		/// <returns>Returns <see langword="true"/> if the operation is removed; <see langword="false"/> otherwise.</returns>
-		/// <seealso cref="Add(AsyncResult)"/>
-		public bool Remove(AsyncResult op)
+		/// <seealso cref="Add(T)"/>
+		public bool Remove(T op)
 		{
 			lock (_ops)
 			{
@@ -113,7 +113,7 @@ namespace UnityFx.Async
 		}
 
 		/// <inheritdoc/>
-		public bool Contains(AsyncResult op)
+		public bool Contains(T op)
 		{
 			lock (_ops)
 			{
@@ -122,7 +122,7 @@ namespace UnityFx.Async
 		}
 
 		/// <inheritdoc/>
-		public void CopyTo(AsyncResult[] array, int arrayIndex)
+		public void CopyTo(T[] array, int arrayIndex)
 		{
 			lock (_ops)
 			{
@@ -144,7 +144,7 @@ namespace UnityFx.Async
 		#region IEnumerable
 
 		/// <inheritdoc/>
-		public IEnumerator<AsyncResult> GetEnumerator()
+		public IEnumerator<T> GetEnumerator()
 		{
 			return _ops.GetEnumerator();
 		}
@@ -163,7 +163,7 @@ namespace UnityFx.Async
 		{
 			lock (_ops)
 			{
-				_ops.Remove(sender as AsyncResult);
+				_ops.Remove(sender as T);
 
 				if (_ops.Count > 0)
 				{
