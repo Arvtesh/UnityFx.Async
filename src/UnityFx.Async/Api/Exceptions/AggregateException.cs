e24d29c01ee78ec4a0b1d3efcbfd85f631736168
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-02-14 16:03:20 +0200

    AggregateException implementation for net35

--- src/UnityFx.Async/Api/Exceptions/AggregateException.cs
+++ src/UnityFx.Async/Api/Exceptions/AggregateException.cs
@@ -3,6 +3,10 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Security;
 
 namespace UnityFx.Async
 {
@@ -12,10 +16,13 @@ namespace UnityFx.Async
 	/// Represents one or more errors that occur during application execution.
 	/// </summary>
 	/// <seealso cref="IAsyncOperation"/>
-	public class AggregateException : Exception
+	[Serializable]
+	[DebuggerDisplay("{DebuggerDisplay,nq}")]
+	public sealed class AggregateException : Exception
 	{
 		#region data
 
+		private const string _exceptionsName = "_exceptions";
 		private readonly List<Exception> _exceptions = new List<Exception>();
 
 		#endregion
@@ -40,6 +47,66 @@ namespace UnityFx.Async
 		public AggregateException(IEnumerable<Exception> exceptions)
 		{
 			_exceptions.AddRange(exceptions);
+		}
+
+		#endregion
+
+		#region ISerializable
+
+		[SecurityCritical]
+		private AggregateException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+			var innerExceptions = info.GetValue(_exceptionsName, typeof(Exception[])) as Exception[];
+			_exceptions.AddRange(innerExceptions);
+		}
+
+		/// <inheritdoc/>
+		[SecurityCritical]
+		public override void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			base.GetObjectData(info, context);
+
+			var innerExceptions = new Exception[_exceptions.Count];
+			_exceptions.CopyTo(innerExceptions, 0);
+			info.AddValue(_exceptionsName, innerExceptions, typeof(Exception[]));
+		}
+
+		#endregion
+
+		#region Obejct
+
+		/// <inheritdoc/>
+		public override string ToString()
+		{
+			var text = base.ToString();
+
+			for (var i = 0; i < _exceptions.Count; ++i)
+			{
+				text = string.Format(
+					CultureInfo.InvariantCulture,
+					"{0}{1}{2}: {3}{4}{5}",
+					text,
+					Environment.NewLine,
+					i,
+					_exceptions[i].ToString(),
+					"<---",
+					Environment.NewLine);
+			}
+
+			return text;
+		}
+
+		#endregion
+
+		#region implementation
+
+		private string DebuggerDisplay
+		{
+			get
+			{
+				return "Count = " + _exceptions.Count.ToString(CultureInfo.InvariantCulture);
+			}
 		}
 
 		#endregion
