4e11063599a2eaeebd2b4d3ff4c1b7db4340cf6f
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-04-06 15:52:08 +0300

    Added Finally tests

--- src/UnityFx.Async.Tests/Tests/CatchTests.cs
+++ src/UnityFx.Async.Tests/Tests/CatchTests.cs
@@ -10,5 +10,89 @@ namespace UnityFx.Async
 {
 	public class CatchTests
 	{
+		[Fact]
+		public async Task Catch_DelegateIsNotCalledOnSuccess()
+		{
+			// Arrange
+			var called = false;
+			var op = AsyncResult.Delay(1).Catch(e => called = true);
+
+			// Act
+			await op;
+
+			// Assert
+			Assert.False(called);
+		}
+
+		[Fact]
+		public async Task Catch_DelegateIsCalledOnFailure()
+		{
+			// Arrange
+			Exception exception = null;
+			var op0 = AsyncResult.FromException(new Exception());
+			var op = op0.Catch(e => exception = e);
+
+			// Act
+			await op;
+
+			// Assert
+			Assert.Equal(op0.Exception.InnerException, exception);
+		}
+
+		[Fact]
+		public async Task Catch_DelegateIsCalledOnCancel()
+		{
+			// Arrange
+			var called = false;
+			var op = AsyncResult.FromCanceled().Catch(e => called = true);
+
+			// Act
+			await op;
+
+			// Assert
+			Assert.True(called);
+		}
+
+		[Fact]
+		public async Task Catch_ExceptionIsFiltered()
+		{
+			// Arrange
+			var called = false;
+			var op = AsyncResult.FromCanceled().Catch<OperationCanceledException>(e => called = true);
+
+			// Act
+			await op;
+
+			// Assert
+			Assert.True(called);
+		}
+
+		[Fact]
+		public async Task Catch_ExceptionIsFiltered_2()
+		{
+			// Arrange
+			var called = false;
+			var op = AsyncResult.FromCanceled().Catch<Exception>(e => called = true);
+
+			// Act
+			await op;
+
+			// Assert
+			Assert.True(called);
+		}
+
+		[Fact]
+		public async Task Catch_ExceptionIsFiltered_3()
+		{
+			// Arrange
+			var called = false;
+			var op = AsyncResult.FromCanceled().Catch<InvalidOperationException>(e => called = true);
+
+			// Act
+			await op;
+
+			// Assert
+			Assert.False(called);
+		}
 	}
 }
