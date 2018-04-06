3626b7de3fd43c3fde57bfd794ad6bb77340fec4
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-04-06 16:12:26 +0300

    Added Then tests

--- src/UnityFx.Async.Tests/Tests/ThenTests.cs
+++ src/UnityFx.Async.Tests/Tests/ThenTests.cs
@@ -10,5 +10,58 @@ namespace UnityFx.Async
 {
 	public class ThenTests
 	{
+		[Fact]
+		public async Task Then_DelegateIsCalledOnSuccess()
+		{
+			// Arrange
+			var called = false;
+			var op = AsyncResult.Delay(1).Then(() => called = true);
+
+			// Act
+			await op;
+
+			// Assert
+			Assert.True(called);
+		}
+
+		[Fact]
+		public async Task Then_DelegateIsNotCalledOnFailure()
+		{
+			// Arrange
+			var called = false;
+			var op = AsyncResult.FromException(new Exception()).Then(() => called = true);
+
+			// Act
+			try
+			{
+				await op;
+			}
+			catch
+			{
+			}
+
+			// Assert
+			Assert.False(called);
+		}
+
+		[Fact]
+		public async Task Then_DelegateIsNotCalledOnCancel()
+		{
+			// Arrange
+			var called = false;
+			var op = AsyncResult.FromCanceled().Then(() => called = true);
+
+			// Act
+			try
+			{
+				await op;
+			}
+			catch
+			{
+			}
+
+			// Assert
+			Assert.False(called);
+		}
 	}
 }
