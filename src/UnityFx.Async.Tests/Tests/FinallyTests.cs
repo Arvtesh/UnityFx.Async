8e91000f38bfd6515d747e411b605f5e9709b529
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-04-06 15:55:00 +0300

    Finally tests added

--- src/UnityFx.Async.Tests/Tests/FinallyTests.cs
+++ src/UnityFx.Async.Tests/Tests/FinallyTests.cs
@@ -10,5 +10,46 @@ namespace UnityFx.Async
 {
 	public class FinallyTests
 	{
+		[Fact]
+		public async Task Finally_DelegateIsCalledOnSuccess()
+		{
+			// Arrange
+			var called = false;
+			var op = AsyncResult.Delay(1).Finally(() => called = true);
+
+			// Act
+			await op;
+
+			// Assert
+			Assert.True(called);
+		}
+
+		[Fact]
+		public async Task Finally_DelegateIsCalledOnFailure()
+		{
+			// Arrange
+			var called = false;
+			var op = AsyncResult.FromException(new Exception()).Finally(() => called = true);
+
+			// Act
+			await op;
+
+			// Assert
+			Assert.True(called);
+		}
+
+		[Fact]
+		public async Task Finally_DelegateIsCalledOnCancel()
+		{
+			// Arrange
+			var called = false;
+			var op = AsyncResult.FromCanceled().Finally(() => called = true);
+
+			// Act
+			await op;
+
+			// Assert
+			Assert.True(called);
+		}
 	}
 }
