3cd51ebb869a20b89df21a0669d918cc223d6f83
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-04-23 12:12:24 +0300

    Added a few more tests

--- src/UnityFx.Async.Tests/Tests/CompletionCallbackTests.cs
+++ src/UnityFx.Async.Tests/Tests/CompletionCallbackTests.cs
@@ -68,18 +68,21 @@ namespace UnityFx.Async
 
 			void TestMethod()
 			{
-				for (var i = 0; i < 1000; ++i)
+				for (var i = 0; i < 10000; ++i)
 				{
 					op.TryAddCompletionCallback(CompletionCallback);
 				}
 			}
 
 			// Act
-			await Task.WhenAll(Task.Run(new Action(TestMethod)), Task.Run(new Action(TestMethod)), Task.Run(new Action(TestMethod)));
+			await Task.WhenAll(
+				Task.Run(new Action(TestMethod)),
+				Task.Run(new Action(TestMethod)),
+				Task.Run(new Action(TestMethod)));
 
 			// Assert
 			op.SetCompleted();
-			Assert.Equal(3000, counter);
+			Assert.Equal(30000, counter);
 		}
 
 		[Fact]
@@ -95,6 +98,58 @@ namespace UnityFx.Async
 
 			// Assert
 			continuation.Received(1).Invoke(op, false);
+			continuation.Received(0).Invoke(op, true);
+		}
+
+		[Fact]
+		public void AddContinuation_ExecutesIfOperationIsCompletedSynchronously()
+		{
+			// Arrange
+			var op = AsyncResult.CompletedOperation;
+			var continuation = Substitute.For<IAsyncContinuation>();
+
+			// Act
+			op.AddContinuation(continuation);
+
+			// Assert
+			continuation.Received(1).Invoke(op, true);
+			continuation.Received(0).Invoke(op, false);
+		}
+
+		[Fact]
+		public void AddCompletionCallback_ExecutesIfOperationIsCompletedSynchronously()
+		{
+			// Arrange
+			var op = AsyncResult.CanceledOperation;
+			var callbackCalled = false;
+
+			// Act
+			op.AddCompletionCallback(_ => callbackCalled = true, null);
+
+			// Assert
+			Assert.True(callbackCalled);
+		}
+
+		[Fact]
+		public async Task TryAddCompletionCallback_ContinuationsAreRunOnCorrectSynchronozationContext()
+		{
+			// Arrange
+			var op = new AsyncCompletionSource();
+			var op2 = new AsyncCompletionSource();
+			var sc = Substitute.For<SynchronizationContext>();
+			var tid = 0;
+			var tidActual = 0;
+
+			op.TryAddCompletionCallback(_ => { }, sc);
+			op2.TryAddCompletionCallback(_ => tidActual = Thread.CurrentThread.ManagedThreadId, null);
+
+			// Act
+			await Task.Run(() => op.SetCompleted());
+			await Task.Run(() => { tid = Thread.CurrentThread.ManagedThreadId; op2.SetCompleted(); });
+
+			// Assert
+			sc.Received(1).Post(Arg.Any<SendOrPostCallback>(), Arg.Any<object>());
+			Assert.Equal(tid, tidActual);
 		}
 	}
 }
