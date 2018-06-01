9e9efe84e8a8e120194ac9c0f7e607150daea2b3
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-06-01 19:13:07 +0300

    A few renames

---
+++ src/UnityFx.Async.Tests/Tests/RetryTests.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnityFx.Async
+{
+	public class RetryTests
+	{
+	}
+}
