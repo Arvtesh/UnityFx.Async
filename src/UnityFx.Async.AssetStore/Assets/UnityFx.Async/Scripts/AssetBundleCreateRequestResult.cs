f273f550d02ff6f7e4f85c1e645cc46a0d9f9323
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-06-22 14:52:42 +0300

    Added wrapper for AssetBundleCreateRequest operation

---
+++ src/UnityFx.Async.AssetStore/Assets/UnityFx.Async/Scripts/AssetBundleCreateRequestResult.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using UnityEngine;
+
+namespace UnityFx.Async
+{
+	/// <summary>
+	/// A wrapper for <see cref="AssetBundleCreateRequest"/>.
+	/// </summary>
+	public class AssetBundleCreateRequestResult : AsyncOperationResult<AssetBundle>
+	{
+		#region data
+		#endregion
+
+		#region interface
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AssetBundleCreateRequestResult"/> class.
+		/// </summary>
+		/// <param name="op">Source web request.</param>
+		public AssetBundleCreateRequestResult(AssetBundleCreateRequest op)
+			: base(op)
+		{
+		}
+
+		#endregion
+
+		#region AsyncResult
+
+		/// <inheritdoc/>
+		protected override AssetBundle GetResult(AsyncOperation op)
+		{
+			return (op as AssetBundleCreateRequest).assetBundle;
+		}
+
+		#endregion
+	}
+}
