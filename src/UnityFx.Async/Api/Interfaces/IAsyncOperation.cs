6b0a68ef8280cd5472eb313f9df2ba622ccf016b
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-07-30 12:03:46 +0300

    Moved AsyncOperationStatus to own file

--- src/UnityFx.Async/Api/Interfaces/IAsyncOperation.cs
+++ src/UnityFx.Async/Api/Interfaces/IAsyncOperation.cs
@@ -6,44 +6,6 @@ using System;
 namespace UnityFx.Async
 {
 	/// <summary>
-	/// Enumerates possible status values for <see cref="IAsyncOperation"/>.
-	/// </summary>
-	/// <seealso href="https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.taskstatus">TaskStatus</seealso>
-	/// <seealso cref="IAsyncOperation"/>
-	public enum AsyncOperationStatus
-	{
-		/// <summary>
-		/// The operation is initialized but has not yet been scheduled for execution.
-		/// </summary>
-		Created = AsyncResult.StatusCreated,
-
-		/// <summary>
-		/// The operation has been scheduled for execution but has not yet begun executing.
-		/// </summary>
-		Scheduled = AsyncResult.StatusScheduled,
-
-		/// <summary>
-		/// The operation is running but has not yet completed.
-		/// </summary>
-		Running = AsyncResult.StatusRunning,
-
-		/// <summary>
-		/// The operation completed execution successfully.
-		/// </summary>
-		RanToCompletion = AsyncResult.StatusRanToCompletion,
-
-		/// <summary>
-		/// The operation has been canceled.
-		/// </summary>
-		Canceled = AsyncResult.StatusCanceled,
-
-		/// <summary>
-		/// The operation completed due to an unhandled exception.
-		/// </summary>
-		Faulted = AsyncResult.StatusFaulted
-	}
-
-	/// <summary>
 	/// Represents the consumer side of an asynchronous operation. It is basically a disposable/hookable <see cref="IAsyncResult"/> with status information.
 	/// </summary>
 	/// <seealso href="https://docs.microsoft.com/en-us/dotnet/api/system.threading.tasks.task">Task</seealso>
