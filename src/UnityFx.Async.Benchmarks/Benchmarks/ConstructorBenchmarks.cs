28dbb5ca46e5f9aad48e79314edfb457ad6436d0
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-09-11 19:12:54 +0300

    Added benchmarks project

---
+++ src/UnityFx.Async.Benchmarks/Benchmarks/ConstructorBenchmarks.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace UnityFx.Async.Benchmarks
+{
+	[MemoryDiagnoser]
+	public class ConstructorBenchmarks
+	{
+		private readonly AsyncCallback _asyncCallback = op => { };
+		private readonly Action _action = () => { };
+
+		[Benchmark]
+		public object CreateAsync()
+		{
+			return new AsyncResult(_asyncCallback, null);
+		}
+
+		[Benchmark]
+		public object CreateAsyncCompletionSource()
+		{
+			return new AsyncCompletionSource<object>();
+		}
+
+		[Benchmark]
+		public object CreateTask()
+		{
+			return new Task(_action);
+		}
+
+		[Benchmark]
+		public object CreateTaskCompletionSource()
+		{
+			return new TaskCompletionSource<object>();
+		}
+	}
+}
---
+++ src/UnityFx.Async.Benchmarks/Benchmarks/ContinueWithBenchmarks.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace UnityFx.Async.Benchmarks
+{
+	[MemoryDiagnoser]
+	public class ContinueWithBenchmarks
+	{
+		private readonly Action<IAsyncOperation<object>> _action1 = op => { };
+		private readonly Action<Task<object>> _action2 = task => { };
+
+		[Params(1, 2, 4, 8, 16)]
+		public int N { get; set; }
+
+		[Benchmark]
+		public void Async_ContinueWith()
+		{
+			var acp = new AsyncCompletionSource<object>();
+
+			for (int i = 0; i < N; i++)
+			{
+				acp.Operation.ContinueWith(_action1);
+			}
+		}
+
+		[Benchmark]
+		public void Task_ContinueWith()
+		{
+			var tcs = new TaskCompletionSource<object>();
+
+			for (int i = 0; i < N; i++)
+			{
+				tcs.Task.ContinueWith(_action2);
+			}
+		}
+	}
+}
