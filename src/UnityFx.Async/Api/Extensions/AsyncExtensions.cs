0123457a18d2d22b1c2f9b311120b3875340f06b
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2018-11-01 19:40:11 +0200

    Added SynchronizationContext extensions (async versions of Send/Post)

--- src/UnityFx.Async/Api/Extensions/AsyncExtensions.cs
+++ src/UnityFx.Async/Api/Extensions/AsyncExtensions.cs
@@ -270,6 +270,32 @@ namespace UnityFx.Async
 		}
 
 		/// <summary>
+		/// Dispatches an synchronous message to a synchronization context.
+		/// </summary>
+		/// <param name="context">The target context.</param>
+		/// <param name="action">The delegate to invoke.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="action"/> is <see langword="null"/>.</exception>
+		/// <returns>Returns result of the <paramref name="action"/> call.</returns>
+		public static T Send<T>(this SynchronizationContext context, Func<T> action)
+		{
+			if (action == null)
+			{
+				throw new ArgumentNullException(nameof(action));
+			}
+
+			var result = default(T);
+
+			context.Send(
+				state =>
+				{
+					result = ((Func<T>)state)();
+				},
+				action);
+
+			return result;
+		}
+
+		/// <summary>
 		/// Dispatches an asynchronous message to a synchronization context.
 		/// </summary>
 		/// <param name="context">The target context.</param>
@@ -288,6 +314,85 @@ namespace UnityFx.Async
 			}
 
 			context.Post(_actionCallback, action);
+		}
+
+		/// <summary>
+		/// Dispatches an asynchronous message to a synchronization context.
+		/// </summary>
+		/// <param name="context">The target context.</param>
+		/// <param name="action">The delegate to invoke.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="action"/> is <see langword="null"/>.</exception>
+		/// <returns>An <see cref="IAsyncOperation"/> that can be used to track the operation status.</returns>
+		public static IAsyncOperation PostAsync(this SynchronizationContext context, Action action)
+		{
+			if (action == null)
+			{
+				throw new ArgumentNullException(nameof(action));
+			}
+
+			var op = new ActionResult(action);
+
+			context.Post(
+				state =>
+				{
+					((ActionResult)state).Start();
+				},
+				op);
+
+			return op;
+		}
+
+		/// <summary>
+		/// Dispatches an asynchronous message to a synchronization context.
+		/// </summary>
+		/// <param name="context">The target context.</param>
+		/// <param name="d">The delegate to invoke.</param>
+		/// <param name="state">User-defined state.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="d"/> is <see langword="null"/>.</exception>
+		/// <returns>An <see cref="IAsyncOperation"/> that can be used to track the operation status.</returns>
+		public static IAsyncOperation PostAsync(this SynchronizationContext context, SendOrPostCallback d, object state)
+		{
+			if (d == null)
+			{
+				throw new ArgumentNullException(nameof(d));
+			}
+
+			var op = new ActionResult(d, state);
+
+			context.Post(
+				s =>
+				{
+					((ActionResult)s).Start();
+				},
+				op);
+
+			return op;
+		}
+
+		/// <summary>
+		/// Dispatches an asynchronous message to a synchronization context.
+		/// </summary>
+		/// <param name="context">The target context.</param>
+		/// <param name="action">The delegate to invoke.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="action"/> is <see langword="null"/>.</exception>
+		/// <returns>An <see cref="IAsyncOperation{TResult}"/> that can be used to track the operation status.</returns>
+		public static IAsyncOperation<T> PostAsync<T>(this SynchronizationContext context, Func<T> action)
+		{
+			if (action == null)
+			{
+				throw new ArgumentNullException(nameof(action));
+			}
+
+			var op = new ActionResult<T>(action);
+
+			context.Post(
+				state =>
+				{
+					((ActionResult<T>)state).Start();
+				},
+				op);
+
+			return op;
 		}
 
 		/// <summary>
@@ -317,6 +422,25 @@ namespace UnityFx.Async
 		/// Dispatches a message to a synchronization context.
 		/// </summary>
 		/// <param name="context">The target context.</param>
+		/// <param name="action">The delegate to invoke.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="action"/> is <see langword="null"/>.</exception>
+		/// <returns>An <see cref="IAsyncOperation"/> that can be used to track the operation status.</returns>
+		public static IAsyncOperation InvokeAsync(this SynchronizationContext context, Action action)
+		{
+			if (context == SynchronizationContext.Current)
+			{
+				return AsyncResult.FromAction(action);
+			}
+			else
+			{
+				return PostAsync(context, action);
+			}
+		}
+
+		/// <summary>
+		/// Dispatches a message to a synchronization context.
+		/// </summary>
+		/// <param name="context">The target context.</param>
 		/// <param name="d">The delegate to invoke.</param>
 		/// <param name="state">User-defined state.</param>
 		/// <exception cref="ArgumentNullException">Thrown if <paramref name="d"/> is <see langword="null"/>.</exception>
@@ -334,6 +458,45 @@ namespace UnityFx.Async
 			else
 			{
 				context.Post(d, state);
+			}
+		}
+
+		/// <summary>
+		/// Dispatches a message to a synchronization context.
+		/// </summary>
+		/// <param name="context">The target context.</param>
+		/// <param name="d">The delegate to invoke.</param>
+		/// <param name="state">User-defined state.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="d"/> is <see langword="null"/>.</exception>
+		/// <returns>An <see cref="IAsyncOperation"/> that can be used to track the operation status.</returns>
+		public static IAsyncOperation InvokeAsync(this SynchronizationContext context, SendOrPostCallback d, object state)
+		{
+			if (context == SynchronizationContext.Current)
+			{
+				return AsyncResult.FromAction(d, state);
+			}
+			else
+			{
+				return PostAsync(context, d, state);
+			}
+		}
+
+		/// <summary>
+		/// Dispatches a message to a synchronization context.
+		/// </summary>
+		/// <param name="context">The target context.</param>
+		/// <param name="action">The delegate to invoke.</param>
+		/// <exception cref="ArgumentNullException">Thrown if <paramref name="action"/> is <see langword="null"/>.</exception>
+		/// <returns>An <see cref="IAsyncOperation{TResult}"/> that can be used to track the operation status.</returns>
+		public static IAsyncOperation<T> InvokeAsync<T>(this SynchronizationContext context, Func<T> action)
+		{
+			if (context == SynchronizationContext.Current)
+			{
+				return AsyncResult.FromAction(action);
+			}
+			else
+			{
+				return PostAsync(context, action);
 			}
 		}
 
---
+++ src/UnityFx.Async/Implementation/Specialized/ActionResult{T}.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace UnityFx.Async
+{
+	internal sealed class ActionResult<T> : AsyncResult<T>
+	{
+		#region data
+
+		private readonly Func<T> _action;
+
+		#endregion
+
+		#region interface
+
+		public ActionResult(Func<T> action)
+			: base(AsyncOperationStatus.Scheduled)
+		{
+			_action = action;
+		}
+
+		#endregion
+
+		#region AsyncResult
+
+		protected override void OnStarted()
+		{
+			try
+			{
+				TrySetResult(_action());
+			}
+			catch (Exception e)
+			{
+				TrySetException(e);
+			}
+		}
+
+		#endregion
+	}
+}
