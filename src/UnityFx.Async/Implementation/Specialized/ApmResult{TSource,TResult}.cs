2d26ddd49a3b44057656cc36af0f44bdb6c4ae3c
Alexander Bogarsukov <a.bogarsukov@gmail.com>
2018-11-03 15:43:43 +0200

    Added WebRequest extension methods

---
+++ src/UnityFx.Async/Implementation/Specialized/ApmResult{TSource,TResult}.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Alexander Bogarsukov.
+// Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
+
+namespace UnityFx.Async
+{
+	internal class ApmResult<TSource, TResult> : AsyncResult<TResult>
+	{
+		public TSource Source { get; }
+
+		public ApmResult(TSource source)
+			: base(AsyncOperationStatus.Running)
+		{
+			Source = source;
+		}
+	}
+}
