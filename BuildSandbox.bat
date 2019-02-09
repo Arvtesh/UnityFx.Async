cb9e8ea1b70388698ae867f9bf2f1a5506ddf9c4
Alexander Bogarsukov <a.bogarsukov@gmail.com>
2019-02-09 23:04:31 +0200

    Added some code to Sandbox

--- BuildSandbox.bat
+++ BuildSandbox.bat
@@ -1,5 +1,5 @@
 powershell .\src\Build.ps1 Release
 rd unity\Sandbox\Assets\Plugins /S /Q
-xcopy bin\AssetStore\netstandard2.0 unity\Sandbox /S
+xcopy bin\AssetStoreLegacy\netstandard2.0 unity\Sandbox /S
 rd unity\Sandbox35\Assets\Plugins /S /Q
-xcopy bin\AssetStore\net35 unity\Sandbox35 /S
+xcopy bin\AssetStoreLegacy\net35 unity\Sandbox35 /S
