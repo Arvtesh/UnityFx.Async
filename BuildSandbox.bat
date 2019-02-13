4287788bdf5697cc6ce0b3ea3ad6aa6310761bab
Alexander Bogarsukov <a.bogarsukov@program-ace.com.ua>
2019-02-13 18:16:08 +0200

    Removed Sandbox35

--- BuildSandbox.bat
+++ BuildSandbox.bat
@@ -1,5 +1,3 @@
 powershell .\src\Build.ps1 Release
 rd unity\Sandbox\Assets\Plugins /S /Q
 xcopy bin\AssetStoreLegacy\netstandard2.0 unity\Sandbox /S
-rd unity\Sandbox35\Assets\Plugins /S /Q
-xcopy bin\AssetStoreLegacy\net35 unity\Sandbox35 /S
